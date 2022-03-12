21494a27448ad09a4d648cad83044e180e9ed2ed
Bruno Silva <bruno3dcontato@gmail.com>
2022-03-12 15:37:11 -0300

    chore: update zx scripts

--- scripts/balance.mjs
+++ scripts/balance.mjs
@@ -2,6 +2,12 @@
 
 import 'zx/globals';
 
-for (let idx = 0; idx < 7; idx++) {
-  $`cada wallet balance ${idx + 1}`;
+const accounts = [`1`, `2` /*, `3`, `4`, `5`, `6`, `7`*/];
+
+for (const account of accounts) {
+  $`cada wallet balance ${account}`;
 }
+
+// for (let idx = 0; idx < 7; idx++) {
+//   $`cada wallet balance ${idx + 1}`;
+// }
